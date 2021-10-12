bfcc5e502d483897e01ae9aa9738315845c4880d
lab <navot@volcani.agri.gov.il>
2021-10-12 09:11:11 +0300

    OvenProcess.py protect get_last_meas from disconnection

--- devices/Oven/OvenProcess.py
+++ devices/Oven/OvenProcess.py
@@ -1,24 +1,28 @@
 import multiprocessing as mp
 import threading as th
+from collections import deque
 from pathlib import Path
 from time import sleep, time_ns
 
+import numpy as np
 import pandas as pd
+from devices.Oven.PyCampbellCR1000.device import CR1000
+from devices.Oven.PyCampbellCR1000.exceptions import NoDeviceException
 from serial import SerialException, SerialTimeoutException
+from serial.tools.list_ports import comports
+from tqdm import tqdm
 
 from devices import DeviceAbstract
-from devices.Oven.utils import get_last_measurements
+from devices.Camera.CameraProcess import CameraCtrl
+from devices.Oven.utils import get_last_measurements, make_temperature_offset
 from utils.constants import *
 from utils.logger import make_logging_handlers
-from utils.misc import get_time
+from utils.misc import tqdm_waiting, get_time
 
 OVEN_RECORDS_FILENAME = 'oven_records.csv'
 
 
 def make_oven(logging_handlers: tuple = make_logging_handlers(None, True), logging_level: int = 20):
-    from devices.Oven.PyCampbellCR1000.device import CR1000
-    from utils.misc import get_time
-    from serial.tools.list_ports import comports
     list_ports = comports()
     list_ports = list(filter(lambda x: 'serial' in x.description.lower() and 'usb' in x.device.lower(), list_ports))
     try:
@@ -145,11 +149,15 @@ class OvenCtrl(DeviceAbstract):
 
         while self._flag_run:
             self._semaphore_collect.acquire()
-            if not (records := get_last_measurements(self._oven)):
-                continue
+            try:
+                if not (records := get_last_measurements(self._oven)):
+                    continue
+            except (NoDeviceException, RuntimeError, ValueError, AttributeError, IOError, KeyError):
+                self._oven.log.critical('Oven not connected.')
+                return
             records[DATETIME] = get_time()  # update inaccurate oven time
-            records[T_FPA] = float(self._temperatures[T_FPA])
-            records[T_HOUSING] = float(self._temperatures[T_HOUSING])
+            records[T_FPA] = float(self._temperatures[T_FPA]) / 100
+            records[T_HOUSING] = float(self._temperatures[T_HOUSING]) / 100
             records = pd.DataFrame(records, index=[records.pop(DATETIME)])
             df = pd.concat([df, pd.DataFrame(records)], ignore_index=False).drop_duplicates().sort_values('RecNbr')
             df.to_csv(self._records_path, index_label=DATETIME)
@@ -185,9 +193,55 @@ class OvenCtrl(DeviceAbstract):
             pass
         try:
             self._semaphore_collect.release()
+            self._semaphore_collect.release()
         except (ValueError, TypeError, AttributeError, RuntimeError, NameError, KeyError):
             pass
         try:
             self._semaphore_setpoint.release()
+            self._semaphore_setpoint.release()
         except (ValueError, TypeError, AttributeError, RuntimeError, NameError, KeyError):
             pass
+        try:
+            self._oven.__del__()
+        except (ValueError, TypeError, AttributeError, RuntimeError, NameError, KeyError):
+            pass
+
+
+def set_oven_and_settle(setpoint: (float, int), settling_time_minutes: int, oven: OvenCtrl, camera: CameraCtrl) -> None:
+    # creates a round-robin queue of differences (dt_camera) to wait until t_camera settles
+    queue_temperatures = deque(maxlen=1 + (60 // PID_FREQ_SEC) * settling_time_minutes)
+    queue_temperatures.append(camera.fpa)  # -inf so that the diff always returns +inf
+    offset = make_temperature_offset(t_next=setpoint, t_oven=oven.temperature(T_FLOOR), t_cam=camera.fpa)
+    oven.setpoint = setpoint + offset  # sets the setpoint with the offset of the oven
+
+    # wait until signal error reaches within 1.5deg of setPoint
+    initial_wait_time = PID_FREQ_SEC + OVEN_LOG_TIME_SECONDS * 4  # to let the average ErrSignal settle
+    tqdm_waiting(initial_wait_time, postfix=f'Initial PID setup time')
+    sleep(0.5)
+    with tqdm(desc=f'Settling near {oven.temperature(SETPOINT)}C') as progressbar:
+        while oven.temperature(SIGNALERROR) >= 1.5:
+            progressbar.set_postfix_str(f'Floor temperature {oven.temperature(T_FLOOR):.2f}C, '
+                                        f'Signal error {oven.temperature(SIGNALERROR):.2f}')
+            sleep(1)
+    oven.setpoint = setpoint  # sets the setpoint to the oven
+    print(f'Waiting for the Camera to settle near {setpoint:.2f}C', flush=True)
+    sleep(1)
+
+    n_minutes_settled = 0
+    with tqdm(total=settling_time_minutes, desc=f'Wait for settling {settling_time_minutes} Minutes',
+              unit_scale=True, unit_divisor=PID_FREQ_SEC) as progressbar:
+        while n_minutes_settled < settling_time_minutes:
+            queue_temperatures.append(camera.fpa)
+            dt = np.mean(np.diff(queue_temperatures))
+            if np.abs(dt) >= 1:
+                n_minutes_settled = 0
+                progressbar.refresh()
+                progressbar.reset()
+            else:
+                n_minutes_settled += PID_FREQ_SEC / 60
+                progressbar.update()
+            progressbar.set_postfix_str(f"FPA {queue_temperatures[-1] / 100:.1f}C, "
+                                        f"Oven {oven.temperature(T_FLOOR)}, dt {dt / 100:.2f}C")
+            sleep(PID_FREQ_SEC)
+    sleep(1)
+    print(f'Camera temperature {camera.fpa:.2f}C and settled after {n_minutes_settled} minutes.', flush=True)
