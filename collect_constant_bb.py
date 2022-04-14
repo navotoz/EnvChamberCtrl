4fc655f81f10dd16f61a5bd1d1e5d2178c3d580a
lab <navot@volcani.agri.gov.il>
2022-04-14 10:24:07 +0300

    progressbar with fpa

--- collect_constant_bb.py
+++ collect_constant_bb.py
@@ -115,6 +115,7 @@ if __name__ == "__main__":
             dict_meas.setdefault(T_FPA, []).append(fpa)
             dict_meas.setdefault(T_HOUSING, []).append(camera.housing)
             sleep(rate_sleep_value)  # limits the Hz of the camera
+            progressbar.set_postfix_str(f'FPA {fpa / 100}C')
             progressbar.update()
     oven.setpoint = 0  # turn the oven off
     pickle.dump(dict_meas, open(str(path_to_save / filename), 'wb'))
