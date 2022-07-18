a2bf111b172ddf4818d08c51f0122ed020093d4d
lab <navot@volcani.agri.gov.il>
2022-07-18 11:43:50 +0300

    ffc before meas blocking

--- collect_varying_bb_and_fpa.py
+++ collect_varying_bb_and_fpa.py
@@ -28,21 +28,8 @@ def th_t_cam_getter():
         sleep(TEMPERATURE_ACQUIRE_FREQUENCY_SECONDS)
 
 
-def th_ffc_on_t():
-    while True:
-        try:
-            fpa = camera.fpa
-            if fpa and fpa >= t_ffc:
-                while not camera.ffc:
-                    sleep(0.5)
-                break
-        except (BrokenPipeError, ValueError, TypeError, AttributeError, RuntimeError):
-            pass
-        finally:
-            sleep(2 * TEMPERATURE_ACQUIRE_FREQUENCY_SECONDS)
-
-
 if __name__ == "__main__":
+    print('\n###### TURN ON BOTH OVEN SWITCHES ######\n')
     args = args_var_bb_fpa()
     bb_generator = TbbGenSawTooth(bb_min=args.blackbody_min, bb_max=args.blackbody_max, bb_start=args.blackbody_start,
                                   bb_inc=args.blackbody_increments, bb_is_decreasing=args.blackbody_is_decreasing)
@@ -61,11 +48,6 @@ if __name__ == "__main__":
     blackbody, camera, oven = init_devices(path_to_save=path_to_save, params=params)
     wait_for_devices_to_start(blackbody, camera, oven)
 
-    # if args.ffc == 0 performs FFC before each measurement. Else perform only on the given temperature
-    t_ffc = args.ffc
-    th_ffc = None if not t_ffc else th.Thread(target=th_ffc_on_t, name='th_ffc_on_t', daemon=True)
-    th_ffc.start() if th_ffc is not None else None
-
     # wait for the records file to be created
     while not (path_to_save / OVEN_RECORDS_FILENAME).is_file():
         sleep(1)
@@ -81,6 +63,23 @@ if __name__ == "__main__":
     # measurements
     oven.setpoint = 120  # the Soft limit of the oven is 120C
     filename = f"{now}.npz" if not args.filename else Path(args.filename).with_suffix('.npz')
+
+    # if args.ffc == 0 performs FFC before each measurement. Else perform only on the given temperature
+    t_ffc = args.ffc
+    if t_ffc != 0:
+        while True:
+            try:
+                fpa = camera.fpa
+                if fpa and fpa >= t_ffc:
+                    while not camera.ffc:
+                        sleep(0.5)
+                    break
+            except (BrokenPipeError, ValueError, TypeError, AttributeError, RuntimeError):
+                pass
+            finally:
+                sleep(2 * TEMPERATURE_ACQUIRE_FREQUENCY_SECONDS)
+
+    # start measurements
     dict_meas = collect_measurements(bb_generator=bb_generator, blackbody=blackbody, camera=camera,
                                      n_samples=args.n_samples, limit_fpa=limit_fpa, t_ffc=t_ffc)
     oven.setpoint = 0  # turn the oven off
