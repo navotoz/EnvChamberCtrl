4abf9e3c3b439f5ee55ff976b5e717ede18f66bc
lab <navot@volcani.agri.gov.il>
2022-11-14 07:53:20 +0200

    rand consolidated

--- collect_rand_bb.py
+++ collect_rand_bb.py
@@ -1,7 +1,8 @@
 import sys
 import sys
 import threading as th
-from multiprocessing import Process
+from itertools import count
+from multiprocessing import Process, Semaphore
 from pathlib import Path
 from time import sleep
 
@@ -13,7 +14,7 @@ from devices.Oven.plots import mp_realttime_plot
 from utils.args import args_rand_bb
 from utils.bb_iterators import TbbGenRand
 from utils.common import collect_measurements, save_results, wait_for_devices_to_start, init_devices, \
-    save_run_parameters
+    save_run_parameters, mp_save_measurements_to_zip, wait_for_fpa, continuous_collection
 
 sys.path.append(str(Path().cwd().parent))
 
@@ -28,29 +29,17 @@ def th_t_cam_getter():
         sleep(TEMPERATURE_ACQUIRE_FREQUENCY_SECONDS)
 
 
-def th_ffc_on_t():
-    while True:
-        try:
-            fpa = camera.fpa
-            if fpa and fpa >= t_ffc:
-                while not camera.ffc:
-                    sleep(0.5)
-                break
-        except (BrokenPipeError, ValueError, TypeError, AttributeError, RuntimeError):
-            pass
-        finally:
-            sleep(2 * TEMPERATURE_ACQUIRE_FREQUENCY_SECONDS)
-
-
 if __name__ == "__main__":
     args = args_rand_bb()
     bb_generator = TbbGenRand(bb_min=args.blackbody_min, bb_max=args.blackbody_max, bins=args.bins)
     if args.n_samples <= 0:
         raise ValueError(f'n_samples must be > 0, got {args.n_samples}')
+    assert args.ffc == 0 or args.ffc > 1000, f'FFC must be either 0 or given in [100C] range, got {args.ffc}'
     params = INIT_CAMERA_PARAMETERS.copy()
     params['tlinear'] = int(args.tlinear)
     params['ffc_mode'] = 'external'
     params['ffc_period'] = 0
+    params['ffc_temp_delta'] = 1000
     params['lens_number'] = args.lens_number
     print(f'Lens Number = {args.lens_number}', flush=True)
     limit_fpa = args.limit_fpa
@@ -59,11 +48,7 @@ if __name__ == "__main__":
     path_to_save, now = save_run_parameters(args.path, params, args)
     blackbody, camera, oven = init_devices(path_to_save=path_to_save, params=params)
     wait_for_devices_to_start(blackbody, camera, oven)
-
-    # if args.ffc == 0 performs FFC before each measurement. Else perform only on the given temperature
-    t_ffc = args.ffc
-    th_ffc = None if not t_ffc else th.Thread(target=th_ffc_on_t, name='th_ffc_on_t', daemon=True)
-    th_ffc.start() if th_ffc is not None else None
+    blackbody.set_temperature_non_blocking(args.blackbody_start)
 
     # wait for the records file to be created
     while not (path_to_save / OVEN_RECORDS_FILENAME).is_file():
@@ -77,9 +62,26 @@ if __name__ == "__main__":
     mp_plot = Process(target=mp_realttime_plot, args=(path_to_save,), name='mp_realtime_plot', daemon=True)
     mp_plot.start()
 
+    # save measurements to zip
+    lock_zip = Semaphore(value=0)
+    mp_zip_saver = Process(target=mp_save_measurements_to_zip, args=(path_to_save, lock_zip, ), name='mp_zip_saver',
+                           daemon=True)
+    mp_zip_saver.start()
+
+    # measurements
     oven.setpoint = 120  # the Soft limit of the oven is 120C
     filename = f"{now}.npz" if not args.filename else Path(args.filename).with_suffix('.npz')
-    dict_meas = collect_measurements(bb_generator=bb_generator, blackbody=blackbody, camera=camera,
-                                     n_samples=args.n_samples, limit_fpa=limit_fpa, t_ffc=t_ffc)
-    save_results(path_to_save=path_to_save, filename=filename, dict_meas=dict_meas)
-    oven.setpoint = 0  # turn the oven off
+
+    t_ffc = wait_for_fpa(t_ffc=args.ffc, camera=camera, wait_time_camera=TEMPERATURE_ACQUIRE_FREQUENCY_SECONDS)
+
+    # start measurements
+    minutes_in_chunk = int(args.minutes_in_chunk)
+    assert minutes_in_chunk > 0, f'argument minutes_in_chunk must be > 0, got {minutes_in_chunk}.'
+    for idx in count(start=1, step=1):
+        if continuous_collection(bb_generator=bb_generator, blackbody=blackbody, camera=camera,
+                                 n_samples=args.n_samples, time_to_collect_minutes=minutes_in_chunk,
+                                 sample_rate=args.sample_rate,
+                                 filename=f"{now}_{idx}.npz", path_to_save=path_to_save, limit_fpa=limit_fpa):
+            oven.setpoint = 0  # turn the oven off
+            limit_fpa = None
+        lock_zip.release()  # release the lock to save the measurements to zip
